639062d673aaecfbbeceeed9ecd0bc245d21a274
Meysam Moghaddam <meysam.mogaddam@gmail.com>
2021-04-04 11:17:14 +0430

    Add project files.

---
+++ AutoComplateSelectList.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoComplate
+{
+  
+    public class AutoComplateSelectList
+    {
+        public string Text { get; set; }
+        public string Value { get; set; }
+        public string ImageAddress { get; set; }
+        public string Active { get; set; }
+    }
+}
---
+++ Helper.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoComplate
+{
+    public static class Helper
+    {
+        public static ExpandoObject CreateExpandoFromObject(this object source)
+        {
+            var result = new ExpandoObject();
+            IDictionary<string, object> dictionary = result;
+            foreach (var property in source
+                .GetType()
+                .GetProperties()
+                .Where(p => p.CanRead && p.GetMethod.IsPublic))
+            {
+                dictionary[property.Name] = property.GetValue(source, null);
+            }
+            return result;
+        }
+    }
+}
